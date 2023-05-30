866646dd88c6c35011b70e28e54b6fd673a65fd0
rutix <goran@domain.tld>
2023-05-30 10:40:12 +0200

    Init

---
+++ lib/url_slug.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "url_slug/version"
+
+module UrlSlug
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/url_slug_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe UrlSlug do
+  it "has a version number" do
+    expect(UrlSlug::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
